66c074c95e0171d18dabbae2f73eef07256246dc
Sanket Sutar <sanket.sutar@tum.de>
2021-06-18 09:58:23 +0000

    Update watch-lecture.tsx

--- libs/watch-lecture/src/lib/watch-lecture.tsx
+++ libs/watch-lecture/src/lib/watch-lecture.tsx
@@ -13,32 +13,6 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-export function WatchLecture(props: WatchLectureProps) {
-  const classes = useStyles();
-  return (
-    <div>
-      <Container className={classes.padded}>
-        <h1>Lecture</h1>
-        <Paper variant="outlined" className={classes.padded}>
-          <h3>Lecture 11: How to create a React page</h3>
-          <Grid container spacing={3}>
-            <Grid item xs={6}>
-              <video controls width="250">
 
-                  <source src="/media/cc0-videos/flower.webm"
-                          type="video/webm">
-
-                  <source src="/media/cc0-videos/flower.mp4"
-                          type="video/mp4">
-
-                  Sorry, your browser does not support embedded videos.
-              </video>
-            </Grid>
-          </Grid>
-        </Paper>
-      </Container>
-    </div>
-  );
-}
 
 export default WatchLecture;
