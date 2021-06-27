99f0cd0b0f31fb1608443bf9ef4cdf11239cda13
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-06-27 19:15:39 +0200

    add sorting select

--- libs/questions/src/lib/question-list/question-list.tsx
+++ libs/questions/src/lib/question-list/question-list.tsx
@@ -1,18 +1,33 @@
 import QuestionListEntry from '../question-list-entry/question-list-entry';
-import { Grid } from '@material-ui/core';
+import { FormControl, Grid, InputLabel, MenuItem, Select } from '@material-ui/core';
 
 /* eslint-disable-next-line */
 export interface QuestionListProps {}
 
 export function QuestionList(props: QuestionListProps) {
   return (
-    <Grid container spacing={3}>
-      {[...Array(7).keys()].map((id) => (
-        <Grid item xs={12}>
-          <QuestionListEntry id={id.toString()} />
+    <>
+      <Grid container spacing={1} justify='space-between'>
+        <Grid item>
+          <h2>All questions</h2>
         </Grid>
-      ))}
-    </Grid>
+        <Grid item xs={2}>
+          <FormControl fullWidth variant="outlined">
+            <InputLabel id="questions-sort-by-label">Sort by</InputLabel>
+            <Select labelId="questions-sort-by-label">
+              <MenuItem value="likes">Most likes</MenuItem>
+              <MenuItem value='time'>Most recent</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3} direction="column">
+        {[...Array(7).keys()].map((id) => (
+          <Grid item>
+            <QuestionListEntry id={id.toString()} />
+          </Grid>
+        ))}
+      </Grid></>
   );
 }
 
